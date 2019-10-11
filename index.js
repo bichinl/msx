f210d1eb15a8b3faf09c80f7216de4edc53ad423
samflix <sambotello.dev@gmail.com>
2019-10-11 02:18:14 -0500

    again, added now.json and port change

--- index.js
+++ index.js
@@ -14,8 +14,4 @@ const server = http.createServer((req, res) => {
   serve(req, res, done);
 });
 
-server.listen(3000, function (){
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+server.listen(3000);
