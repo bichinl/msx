f45948e974a14fab1c5eb66b35485d0092a63700
samflix <sambotello.dev@gmail.com>
2019-10-11 01:58:50 -0500

    added console log to server.listent

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ const server = http.createServer((req, res) => {
   serve(req, res, done);
 });
 
-server.listen(3000);
+server.listen(3000, function (){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
